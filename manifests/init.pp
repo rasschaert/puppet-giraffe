42b45f0a7522ad5e3329776df70d1716d4d783ea
Kenny Rasschaert <kenny@inuits.eu>
2013-11-29 17:00:50 +0100

    Trying to get the rendered template to look right

--- manifests/init.pp
+++ manifests/init.pp
@@ -41,12 +41,7 @@ class giraffe (
     {
       name        =>'Demo',
       refresh     => 5000,
-      description => '#Giraffe : A [Graphite](http://graphite.wikidot.com) \
-Dashboard with a long neck <img class=\'pull-right\' src=\'img/giraffe.png\' />\
-\n\n<br/>\n<a class=\'btn btn-large btn-warning\' \
-href=\'https://github.com/kenhub/giraffe\'><i class=\'icon-github icon-large\'>\
-</i> View project on github</a>\n\n##More?\n\nCheck out the different \
-dashboards for more information about riding giraffes.',
+      description => '#Giraffe : A [Graphite](http://graphite.wikidot.com) Dashboard with a long neck <img class=\'pull-right\' src=\'img/giraffe.png\' />\n\n<br/>\n<a class=\'btn btn-large btn-warning\' href=\'https://github.com/kenhub/giraffe\'><i class=\'icon-github icon-large\'></i> View project on github</a>\n\n##More?\n\nCheck out the different dashboards for more information about riding giraffes.',
       metrics     =>
       [
         {
@@ -58,19 +53,20 @@ dashboards for more information about riding giraffes.',
         },
         {
           alias         => 'signup breakdown',
-          targets       => ['sumSeries(enter.your.graphite.metrics.here)',
-                            'sumSeries(enter.another.graphite.metrics)'],
+          targets       => [
+            'sumSeries(enter.your.graphite.metrics.here)',
+            'sumSeries(enter.another.graphite.metrics)',
+          ],
           description   => 'signup breakdown based on site location',
           renderer      => 'area',
           unstack       => true,
         },
         {
-          alias           =>'Registration breakdown',
-          target          =>'sumSeries(enter.your.graphite.metrics.here)',
-          target          => 'function() { return \'summarize(\
-events.registration.success,\'\' + entire_period() + \'min)\' }',
-          renderer        =>'bar',
-          description     =>'Registrations based on channel',
+          alias           => 'Registration breakdown',
+          target          => 'sumSeries(enter.your.graphite.metrics.here)',
+          target          =>  'function() { return \'summarize(events.registration.success,\'\' + entire_period() + \'min)\' }',
+          renderer        => 'bar',
+          description     => 'Registrations based on channel',
           hover_formatter => 'd3.format(\'03.6g\')',
           null_as         => 0,
         },
