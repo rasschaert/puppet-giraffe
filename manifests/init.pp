1ccabdc0953cbc6215048e6037c8731a78faff3b
Kenny Rasschaert <kenny@inuits.eu>
2013-12-04 10:15:07 +0100

    moved default parameters over to defaults.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -36,46 +36,11 @@
 # Copyright 2013 Kenny Rasschaert, unless otherwise noted.
 #
 class giraffe (
-  $graphite_url    = 'demo',
-  $override_scheme = false,
-  $scheme          = 'spectrum14',
-  $dashboards      = [
-    {
-      name        =>'Demo',
-      refresh     => 5000,
-      description => '#Giraffe : A [Graphite](http://graphite.wikidot.com) Dashboard with a long neck <img class=\'pull-right\' src=\'img/giraffe.png\' />\n\n<br/>\n<a class=\'btn btn-large btn-warning\' href=\'https://github.com/kenhub/giraffe\'><i class=\'icon-github icon-large\'></i> View project on github</a>\n\n##More?\n\nCheck out the different dashboards for more information about riding giraffes.',
-      metrics     =>
-      [
-        {
-          alias             => 'signups',
-          target            => 'sumSeries(enter.your.graphite.metrics.here)',
-          description       => 'New signups to the website',
-          summary           => 'sum',
-          summary_formatter => 'd3.format(\',f\')',
-        },
-        {
-          alias         => 'signup breakdown',
-          targets       => [
-            'sumSeries(enter.your.graphite.metrics.here)',
-            'sumSeries(enter.another.graphite.metrics)',
-          ],
-          description   => 'signup breakdown based on site location',
-          renderer      => 'area',
-          unstack       => true,
-        },
-        {
-          alias           => 'Registration breakdown',
-          target          => 'sumSeries(enter.your.graphite.metrics.here)',
-          target          =>  'function() { return \'summarize(events.registration.success,\'\' + entire_period() + \'min)\' }',
-          renderer        => 'bar',
-          description     => 'Registrations based on channel',
-          hover_formatter => 'd3.format(\'03.6g\')',
-          null_as         => 0,
-        },
-      ]
-    },
-  ],
-) {
+  $graphite_url    = $::giraffe::params::graphite_url,
+  $override_scheme = $::giraffe::params::override_scheme,
+  $scheme          = $::giraffe::params::scheme,
+  $dashboards      = $::giraffe::params::dashboards,
+) inherits giraffe::params {
 
   package { 'giraffe':
     ensure  => present,
